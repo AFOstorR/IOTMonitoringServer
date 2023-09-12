ec0b30f93628e8bbd4992a92634d52351c5c2892
AFOstorR <af.ostor10@uniandes.edu.co>
2023-09-12 17:36:49 -0500

    Cambio IPs

--- IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-bu+)8ft@9+qd*#e#f_s@wkyv2tmq+#!a^3j15h3kjk^jzksu0j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "3.80.158.10"]
+ALLOWED_HOSTS = ["localhost", "54.166.15.99"]
 
 
 # Application definition
@@ -95,7 +95,7 @@ DATABASES = {
         "NAME": "iot_data",  # Nombre de la base de datos
         "USER": "dbadmin",  # Nombre de usuario
         "PASSWORD": "uniandesIOT1234*",  # Contraseña
-        "HOST": "3.93.165.235",  # Dirección IP de la base de datos
+        "HOST": "18.212.231.188",  # Dirección IP de la base de datos
         "PORT": "",  # Puerto de la base de datos
     }
 }
@@ -155,7 +155,7 @@ LOGOUT_REDIRECT_URL = '/login/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Dirección del bróker MQTT
-MQTT_HOST = "34.235.124.34"
+MQTT_HOST = "54.174.99.198"
 
 # Puerto del bróker MQTT
 MQTT_PORT = 8082
